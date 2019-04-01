fb10009204f747a588ada9961b66e0ac0cd9851a
rafaelymz <mingzhi2@andrew.cmu.edu>
2019-03-31 21:17:45 -0400

    Update Calculate_Distance.py

--- src/util/Calculate_Distance.py
+++ src/util/Calculate_Distance.py
@@ -1,5 +1,8 @@
 from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.models import KeyedVectors
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 
 # you need to install gensim and download the glove.6b.200d.txt online
 
@@ -10,6 +13,10 @@ def calculate_distance(s1, s2):
     word2vec_output_file = 'glove.6B.200d.txt.word2vec'
     glove2word2vec(glove_input_file, word2vec_output_file)
 
+    # remove stopwords
+    s1 = remove_stopwords(s1)
+    s2 = remove_stopwords(s2)
+
     # load the Stanford GloVe model
     filename = 'glove.6B.200d.txt.word2vec'
     model = KeyedVectors.load_word2vec_format(filename, binary=False)
@@ -18,3 +25,13 @@ def calculate_distance(s1, s2):
     distance = model.wmdistance(s1, s2)
 
     return distance
+
+
+def remove_stopwords(s):
+    # remove stopwords
+    stopwords = nltk.corpus.stopwords.words("english")
+
+    word_tokens = word_tokenize(s)
+    filtered_sentence = [w.strip() for w in word_tokens if w not in stopwords]
+    text = " ".join(str(x) for x in filtered_sentence)
+    return text
