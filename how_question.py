bb7148ba6fec70605a1a06488711943bcbf04605
Alex Li <qianl2@andrew.cmu.edu>
2019-04-07 12:19:54 -0400

    define question

--- how_question.py
+++ how_question.py
@@ -1,80 +1,79 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[21]:
 
 
 import nltk
 import json
 import spacy
+import re, urllib, json
+from bs4 import BeautifulSoup
 
 
 # In[2]:
 
 
-#read json file
-with open('dev-v2.0.json') as json_data:
-    content = json.load(json_data)
-    json_data.close()
+# #read json file
+# with open('dev-v2.0.json') as json_data:
+#     content = json.load(json_data)
+#     json_data.close()
+
+# #parse json into a list of paragraphs
+# para = []
+# for data in content['data']:
+#     for paragraphs in data['paragraphs']:
+#         para.append(paragraphs['context'])
+
+# #parse paragraph into list of sentences
+# def parse_sentences(paragraph): 
+#     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+#     sentences = sent_detector.tokenize(paragraph.strip())
+#     return sentences
+
+# squad_sentences = []
+# for p in para:
+#     squad_sentences += parse_sentences(p)
 
 
 # In[3]:
 
 
-#parse json into a list of paragraphs
-para = []
-for data in content['data']:
-    for paragraphs in data['paragraphs']:
-        para.append(paragraphs['context'])
+# for s in squad_sentences:
+#      if 'CARDINAL' in get_entity(s) or 'QUANTITY' in get_entity(s):
+#             print(s)
 
 
 # In[4]:
 
 
-#parse paragraph into list of sentences
-def parse_sentences(paragraph): 
-    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    sentences = sent_detector.tokenize(paragraph.strip())
-    return sentences
-
-
-# In[51]:
-
-
-parsed_sentenses = parse_sentences(text)
-
-
-# In[337]:
-
-
 nlp = spacy.load("en_core_web_lg")
 
 
-# In[253]:
+# In[5]:
 
 
-parag1 = []
-for data in content['data']:
-    for paragraphs in data['paragraphs']:
-        for qas in paragraphs['qas']:
-            question = qas['question']
-            if 'HOW MUCH' in question.upper() and not qas['is_impossible']:
-                parag1.append(paragraphs['context'])
-                print(question)
-                print(paragraphs['context'])
+def countable_noun(noun):
+    url = 'https://books.google.com/ngrams/graph?content=many+' + noun + '%2C+much+' + noun + '&year_start=1800&year_end=2000'
+    response = urllib.request.urlopen(url)
+    html = response.read().decode('utf-8')
+
+    try:
+        many_data = json.loads(re.search('\{"ngram": "many ' + noun + '".*?\}', html).group(0))['timeseries']
+        many = sum(many_data) / float(len(many_data))
+    except:
+        many = 0.0
+    try:
+        much_data = json.loads(re.search('\{"ngram": "much ' + noun + '".*?\}', html).group(0))['timeseries']
+        much = sum(much_data) / float(len(much_data))
+    except:
+        much = 0.0
+    if many > much:
+        return True
+    return False
 
 
-# In[ ]:
-
-
-##How Many questions:
-    #Question Type1 : Subj + verb + number + noun
-    #Question Type2 : There is/are + number + noun + in + place
-    #Question type3: noun + with + number + noun phrase
-    #被动句
-
-
-# In[191]:
+# In[41]:
 
 
 def get_nsubj(sentence):
@@ -82,26 +81,14 @@ def get_nsubj(sentence):
     for chunk in doc.noun_chunks:
         if chunk.root.dep_ == 'nsubj':
             return chunk.text
-
-
-# In[414]:
-
+    return ''
 
 def get_ROOT(sentence):
     doc = nlp(sentence)
     for token in doc:
         if token.dep_ == 'ROOT':
             return token.text, token.lemma_,token.tag_
-
-
-# In[416]:
-
-
-get_ROOT(txt)
-
-
-# In[122]:
-
+    return ''
 
 def get_entity(sentences):
     entity = []
@@ -111,9 +98,6 @@ def get_entity(sentences):
     return entity
 
 
-# In[214]:
-
-
 def get_namechunks(sentence):
     chunks = {}
     doc = nlp(sentence)
@@ -121,10 +105,6 @@ def get_namechunks(sentence):
         chunks[chunk.text] = chunk.root.text
         #chunks.append(chunk.text)
     return chunks
-
-
-# In[425]:
-
 
 def get_tense(verb_t):
     # the verb is present third person singular
@@ -140,82 +120,6 @@ def get_tense(verb_t):
         result = ' does '
     return result
 
-
-# In[421]:
-
-
-def gen_question_type1(sentence):
-    root = get_ROOT(sentence)
-    subj =get_nsubj(sentence)
-    obj = ''
-    for chunk in get_namechunks(sentence).keys():
-        #print(chunk)
-        if 'CARDINAL' in get_entity(chunk) or 'QUANTITY' in get_entity(chunk):
-            obj = get_namechunks(sentence)[chunk]
-            #obj = chunk.split()[-1]
-    prep = ''
-    loc = ''
-    if len(get_pps(sentence)) > 0:
-        for pp in get_pps(sentence):
-            if pp.upper().split()[0] in ['DURING','IN','WHITIN','FROM','UNTIL'] and len(get_entity(pp))>0:
-                if 'CARDINAL' in get_entity(pp) or 'DATE' in get_entity(pp) or 'TIME' in get_entity(pp):
-                    prep = pp
-            if pp.upper().split()[0] == 'IN' and len(get_entity(pp))>0:
-                if get_entity(pp)[0] in ['GPE','FAC','ORG','LOC']:
-                    loc = pp
-                    
-    if subj != None:
-        return "How many "+obj+get_tense(root[2])+subj+" "+root[1].lower()+' '+prep.lower()+' '+loc+'?'
-    else:
-        return "How many "+obj+" were "+root[0].lower()+' '+prep.lower()+' '+loc+'?'
-
-
-# In[419]:
-
-
-def gen_question_type2(sentence):
-    obj = ''
-    for chunk in get_namechunks(sentence).keys():
-        if 'CARDINAL' in get_entity(chunk) or 'QUANTITY' in get_entity(chunk):
-            obj = get_namechunks(sentence)[chunk]
-    prep = ''
-    if len(get_pps(sentence)) > 0:
-        for pp in get_pps(sentence):
-            if pp.upper().split()[0] == 'IN' and len(get_entity(pp))>0:
-                if get_entity(pp)[0] in ['GPE','FAC','ORG','LOC']:
-                    prep = pp
-
-    return "How many "+obj+" are there "+prep.lower()+'?'
-
-
-# In[420]:
-
-
-def gen_question_type3(sentence):
-    root = get_ROOT(sentence)
-    subj =get_nsubj(sentence)
-    obj = ''
-    for chunk in get_namechunks(sentence).keys():
-        if 'CARDINAL' in get_entity(chunk) or 'QUANTITY' in get_entity(chunk):
-            obj = get_namechunks(sentence)[chunk]
-
-            #obj = chunk.split()[-1]
-    prep = ''
-    loc = ''
-    if len(get_pps(sentence)) > 0:
-        for pp in get_pps(sentence):
-            if pp.upper().split()[0] in ['DURING','IN','WHITIN','FROM','UNTIL'] and len(get_entity(pp))>0:
-                if 'CARDINAL' in get_entity(pp) or 'DATE' in get_entity(pp) or 'TIME' in get_entity(pp):
-                    prep = pp
-            if pp.upper().split()[0] == 'IN' and len(get_entity(pp))>0:
-                if get_entity(pp)[0] in ['GPE','FAC','ORG','LOC']:
-                    loc = pp
-    return "How much "+obj+get_tense(root[2])+subj+" "+root[1].lower()+' '+prep.lower()+' '+loc+'?'
-
-
-# In[237]:
-
-
 def get_pps(sentence):
     doc = nlp(sentence)
     pps = []
@@ -227,64 +131,115 @@ def get_pps(sentence):
     return pps
 
 
-# In[393]:
+# In[42]:
 
 
-temp = dep_dict.get("ROOT")
-root_word = temp[0]  # the root word
-verb = temp[1]  # the pos tag of the root word
-
-# find the original word in word_Pos dict
-verb_s = word_Pos.get(root_word)[0]
-
-
-# In[408]:
-
-
-txt = "Each year, the southern California area has about 10,000 earthquakes"
-
-
-# In[409]:
-
-
-txt2 ="In southern California there are also twelve cities with more than 200,000 residents and 34 cities over 100,000 in population."
-
-
-# In[410]:
+def gen_question_type1(sentence,obj):
+    root = get_ROOT(sentence)
+    subj =get_nsubj(sentence)
+    prep = ''
+    loc = ''
+    if len(get_pps(sentence)) > 0:
+        for pp in get_pps(sentence):
+            if pp.upper().split()[0] in ['DURING','IN','WHITIN','FROM','UNTIL'] and len(get_entity(pp))>0:
+                if 'DATE' in get_entity(pp) or 'TIME' in get_entity(pp): #'CARDINAL' in get_entity(pp) or
+                    prep = pp
+            if (pp.upper().split()[0] == 'IN' or pp.upper().split()[0] == 'ON' or pp.upper().split()[0] == 'AT') and len(get_entity(pp))>0:
+                if get_entity(pp)[0] in ['GPE','FAC','ORG','LOC']:
+                    loc = pp
+                    
+    if subj != None:
+        return "How many "+obj+get_tense(root[2])+subj+" "+root[1].lower()+' '+prep.lower()+' '+loc+'?'
+    else:
+        return "How many "+obj+" were "+root[0].lower()+' '+prep.lower()+' '+loc+'?'
 
 
-txt3 = "Booth donated $300 million to the university's Booth School of Business"
+# In[43]:
 
 
-# In[422]:
+def gen_question_type2(sentence,obj):
+    prep = ''
+    if len(get_pps(sentence)) > 0:
+        for pp in get_pps(sentence):
+            if pp.upper().split()[0] == 'IN' and len(get_entity(pp))>0:
+                if get_entity(pp)[0] in ['GPE','FAC','ORG','LOC']:
+                    prep = pp
+
+    return "How many "+obj+" are there "+prep.lower()+'?'
 
 
-gen_question_type2(txt2)
+# In[50]:
 
 
-# In[423]:
+def gen_question_type3(sentence,obj):
+    root = get_ROOT(sentence)
+    subj =get_nsubj(sentence)
+    if obj == '' and root == '' :
+        return ''
+    if obj == '' and subj == '' :
+        return ''
+    prep = ''
+    loc = ''
+    if len(get_pps(sentence)) > 0:
+        for pp in get_pps(sentence):
+            if pp.upper().split()[0] in ['DURING','IN','WHITIN','FROM','UNTIL'] and len(get_entity(pp))>0:
+                if 'CARDINAL' in get_entity(pp) or 'DATE' in get_entity(pp) or 'TIME' in get_entity(pp):
+                    prep = pp
+            if (pp.upper().split()[0] == 'IN' or pp.upper().split()[0] == 'ON') and len(get_entity(pp))>0:
+                if get_entity(pp)[0] in ['GPE','FAC','ORG','LOC']:
+                    loc = pp
+
+    return "How much "+obj+get_tense(root[2])+subj+" "+root[1].lower()+' '+prep.lower()+' '+loc+'?'
 
 
-gen_question_type1(txt)
+# In[52]:
 
 
-# In[424]:
+def select_question(sentence):
+    obj = ''
+    for chunk in get_namechunks(sentence).keys():
+        if 'CARDINAL' in get_entity(chunk) or 'QUANTITY' in get_entity(chunk):
+            obj = get_namechunks(sentence)[chunk]
+
+    if obj == '':
+        return gen_question_type3(sentence, obj)
+    
+    if countable_noun(obj):
+        if 'there' in sentence.lower():
+            return gen_question_type2(sentence, obj)
+        else:
+            return gen_question_type1(sentence, obj)
+    else:
+        return gen_question_type3(sentence, obj)
 
 
-gen_question_type3(txt3)
+# In[22]:
 
 
-# In[145]:
+def open_html(filename):
+    soup = BeautifulSoup(open(filename,encoding='utf-8'), "html.parser")
+    page = soup.find_all('p')
+    paragraphs = ''
+    for p in page:
+        paragraphs += p.getText()
+    return paragraphs
+
+def parse_sentences(paragraph): 
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences = sent_detector.tokenize(paragraph.strip())
+    return sentences
 
 
-for sentence in parsed_sentense:
-    for chunk in get_namechunks(sentence):
-        if 'CARDINAL' in get_entity(chunk):
-            #print(chunk+'\n'+sentence)
-            start = sentence.find(chunk)
-            end = sentence.find(chunk) + len(chunk)
-            print(chunk+'\n'+sentence[0:start]+'\n'+sentence[end:])
-            print(get_entity(sentence[0:start]))
-            print(get_entity(sentence[end:]))
-            
+# In[24]:
+
+
+paragraphs = open_html("a2.htm")
+
+
+# In[53]:
+
+
+for sentence in parse_sentences(paragraphs):
+    #print(sentence)
+    print(select_question(sentence))
 
