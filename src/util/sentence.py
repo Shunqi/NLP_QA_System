431d7fcb60787bd54a89a88c03cd2f2b91e79d25
rafaelymz <mingzhi2@andrew.cmu.edu>
2019-04-24 19:13:28 -0400

    Update sentence.py

--- src/util/sentence.py
+++ src/util/sentence.py
@@ -16,45 +16,50 @@ def stanford_parser(sentence):
     dep_list = list(dep.triples())
     return dep_list, pcfg
 
-def Spacy_parser(sentence):
+#input should be a sentence
+def Spacy_parser(sentence): 
+    from collections import Counter
+    import spacy
+    import pandas as pd
+    #from tabulate import tabulate
     
-    # from tabulate import tabulate
-
     word_Pos = {}
     Pos_word = {}
-    dep_dict = {}  # dict for dependency
-    NER = []
+    dep_dict = {} # dict for dependency 
+    ner_list = []
+    NER = False # boolean value
 
-    # nlp = spacy.load('en_core_web_lg')
+    nlp = spacy.load('en_core_web_lg')
 
     doc = nlp(sentence)
+    
     for ent in doc.ents:
-        NER.append([ent.text, ent.label_])
-
-    for token in doc:
-        # print(token.text, token.dep_, token.head.text, token.head.pos_, token.tag_, [child for child in token.children])
-        # result.append([token.text, token.dep_, token.pos_, token.tag_, [child for child in token.children]])
-
+        ner_list.append([ent.text, ent.label_])
+        
+    if not ner_list: # if the list is not empty
+        NER = True
+        
+    for token in doc:        
         # update the dict with the word as key
-        word_Pos.update({token.text: [token.lemma_, token.pos_, token.tag_, [child for child in token.children]]})
-
+        word_Pos.update({token.text:[token.lemma_, token.pos_, token.tag_, [child for child in token.children]]})
+        
         # update the dependency dict
-        dep_dict.update({token.dep_: [token.text, token.tag_]})
-
+        dep_dict.update({token.dep_:[token.text, token.tag_]})
+        
         # update the dict with the Pos tag as key
         if token.tag_ not in Pos_word:
             Pos_word.update({token.tag_: token.text})
-        else:
+        else: 
             temp = []
             value = Pos_word.get(token.tag_)
-            if isinstance(value, list):  # check if the value is a list
+            if isinstance(value, list): # check if the value is a list
                 temp.extend(Pos_word.get(token.tag_))
             else:
                 temp.append(Pos_word.get(token.tag_))
             temp.append(token.text)
-            Pos_word.update({token.tag_: temp})
-
-    return word_Pos, Pos_word, NER, dep_dict
+            Pos_word.update({token.tag_:temp})
+        
+    return word_Pos, Pos_word, NER, dep_dict, doc 
 
 def get_nsubj(sentence):
     doc = nlp(sentence)
